2a728b7169a19029b68364746afc7910cb4877a7
catch77 <1297056296@qq.com>
2019-07-17 14:43:41 +0800

    ADD: add ADD, UPDATE, FIND Company

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,29 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repo.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
+
+    @Autowired
+    private CompanyRepository companyRepository;
     
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return companyRepository.findAll();
     }
     
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
+    }
+
+    @PutMapping(produces = {"application/json"})
+    public Company update(@RequestBody Company company) {
+        return companyRepository.save(company);
     }
 }
---
+++ src/main/java/com/tw/apistackbase/repo/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.tw.apistackbase.repo;
+
+import com.tw.apistackbase.core.Company;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+
+}
